1fe324d59f49f9ae2d000ba7f74dcdeed044b3ba
Max05643 <05643mak@gmail.com>
2024-02-05 21:15:04 +0100

    Add project files.

---
+++ GameServerDefinitions/IGameServer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServerDefinitions
+{
+    /// <summary>
+    /// The interface provided by game server to external components
+    /// </summary>
+    public interface IGameServer
+    {
+    }
+}
---
+++ GameServerImplementation/GameServer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServerImplementation
+{
+    /// <summary>
+    /// Game server for any web-based game
+    /// </summary>
+    public class GameServer : IGameServer
+    {
+    }
+}
